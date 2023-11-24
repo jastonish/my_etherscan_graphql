85f00871f6b6c87ab69353936e1b7da493298a56
jastonish <136875525+jastonish@users.noreply.github.com>
2023-11-23 20:13:59 -0800

    Add files via upload

---
+++ datasource/ethDatasource.js
@@ -0,0 +1,46 @@
+const { RESTDataSource } = require("apollo-datasource-rest");
+
+//Vitalik's Ethereum Address
+const eth_address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+
+//Etherscan Data Source Class
+class EtherDataSource extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = "https://api.etherscan.io/api";
+  }
+
+  async etherBalanceByAddress() {
+    // Get ether balance by address endpoint
+    return this.get(
+      `?module=account&action=balance&address=${eth_address}&tag=latest&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  async totalSupplyOfEther() {
+    // Get total ether supply endpoint
+    return this.get(
+      `?module=stats&action=ethsupply&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  //Paste Code Here For New API Endpoints
+  
+  // Get latest Ethereum price endpoint
+  async getLatestEthereumPrice() {
+    return this.get(
+      `?module=stats&action=ethprice&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+
+  // Gets the estimated number of blocks required for a transaction 
+  // with 2 Gwei gas price to be included in a block
+  async getBlockConfirmationTime() {
+    // Get gas estimate endpoint
+    return this.get(
+      `?module=gastracker&action=gasestimate&gasprice=2000000000&apikey=${process.env.ETHERSCAN_API}`
+    );
+  }
+}
+
+exports = EtherDataSource;
